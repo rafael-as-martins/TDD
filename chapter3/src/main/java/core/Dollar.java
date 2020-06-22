b63896f2f38e66d971e9228c8be4abae54d9a389
“Rafael <fc52043@alunos.fc.ul.pt>
2020-06-22 16:06:46 +0100

    Adding Chapter 3

---
+++ chapter3/src/main/java/core/Dollar.java
@@ -0,0 +1,14 @@
+package core;
+
+public class Dollar {
+
+    public int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
